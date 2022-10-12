811bcfc34b23e98be03a65f14368624c3f387e9c
Kira <kirakulikova255@gmail.com>
2022-10-12 22:58:13 +0300

    if

---
+++ example5_if/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "кира")
+{
+    Console.WriteLine("Привет");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WtiteLine(username);
+}
